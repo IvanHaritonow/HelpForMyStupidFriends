733bbb90c22a11412aae83583494ed86f61ca22d
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-06 20:02:20 +0300

    Create 1.c

---
+++ var1-c6/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int k, i, j;
+    int a[10] = {0};
+    k = 1;
+
+	printf("Введите 10 чисел \n");
+
+	for (i = 1; i <= 10; i++) {
+      scanf("%d", &j);
+      a[i] = j;
+      if (a[i] % 2 == 0 && a[i] < 15 && a[i] > 0) k *= a[i];
+	}
+
+	printf("Произведение этих чисел равно: %f \n", k);
+
+    return 0;
+}
