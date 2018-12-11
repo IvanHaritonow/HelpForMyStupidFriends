b6d1d2b6a29d546a6e13df4bf94b6c5a2b432c4f
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-11 22:21:04 +0300

    Update 1.c

--- var1-c6/1.c
+++ var1-c6/1.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-
- 
 #include <stdlib.h>
 
-
-int main()
-
-{
-
+int main() {
     int a, b, c, i;
     
     c = 1;
@@ -15,25 +9,19 @@ int main()
      printf("Введите количество чисел: \n");
      scanf("%d", &a);
 
-
 	 for (i = 1; i <= a; i++) {
             printf("%f", i);
             printf(" число: %f");
             scanf("%d", &b);
             if (b % 2 == 0 && b < 15 && b > 0) 
             c *= b;
-
 	 }
 
-
 	 printf("Произведение этих чисел равно: %f \n", c);
-
-
+	
     getch();
-
+	
 }
- 
-int main()
     
   
  
