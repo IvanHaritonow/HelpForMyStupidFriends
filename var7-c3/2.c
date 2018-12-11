416e3e677748cc92fb0af001beced29fb2b9d26d
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-11 21:41:22 +0300

    Create 2.c

---
+++ var7-c3/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int y;
+
+      printf("Введите число: ");
+	  scanf("%d", &y);
+	  
+	  if (y % 2 == 0) printf("Число чётное");
+        else printf("Число нечётное");
+	  
+    getch();
+}
