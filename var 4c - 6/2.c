a7760bc38ec111e9eeb7c3f36e2be81052814c35
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-11 22:44:36 +0300

    Update 2.c

--- var 4c - 6/2.c
+++ var 4c - 6/2.c
@@ -4,8 +4,8 @@ int main() {
 	int i, x;
 	double y;
 	
-for (i = -9; i <= 9; i += 3) {
-	 x = i;
+         for (i = -9; i <= 9; i += 3) {
+	    x = i;
 	  printf("При значении х = %d", x);
 	      y = 4 * x * x + 5 * x - 10;
 	  printf(" функция y(x) = %d\n", y);	
