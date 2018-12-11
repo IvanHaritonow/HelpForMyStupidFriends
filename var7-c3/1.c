a7fb495479c3381faf4da6f90dc0e1a6b9c1b5a8
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-11 21:35:11 +0300

    Update 1.c

--- var7-c3/1.c
+++ var7-c3/1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    double y;
+    int x;
+
+    printf("Введите x: ");
+	  scanf("%d", &x);
+	  
+	  if (x > -8) {
+          y = (5 * x * x + 2) / (x + 8);
+	  } else {
+	      y = (2 * x * x + 9);
+	  }
+      
+      printf("Функция y = %f", y);
+    getch();
+}
