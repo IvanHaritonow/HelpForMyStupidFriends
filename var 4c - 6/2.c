ce296cb9f360461172f99ed5bf9d900b5f8b25df
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-11 22:44:19 +0300

    Create 2.c

---
+++ var 4c - 6/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() { 
+	int i, x;
+	double y;
+	
+for (i = -9; i <= 9; i += 3) {
+	 x = i;
+	  printf("При значении х = %d", x);
+	      y = 4 * x * x + 5 * x - 10;
+	  printf(" функция y(x) = %d\n", y);	
+   } 
+  getch();
+ }
