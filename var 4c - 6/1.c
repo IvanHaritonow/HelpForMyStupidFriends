9dffad6ccd6ca46a1d1a6c58994a63403606eea9
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-11 22:37:01 +0300

    Update 1.c

--- var 4c - 6/1.c
+++ var 4c - 6/1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int i, s;
+    s = 1;
+      for (i = -8; i < 4; i++) s *= i;
+    
+    printf("Произведение всех чисел = %d", s);
+    getch();
+}
