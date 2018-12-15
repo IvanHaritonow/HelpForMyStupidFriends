af28b958bf006c680b5f2dbf740c3a6d63d6661f
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-16 00:54:49 +0300

    Update 3.c

--- var7-c3/3.c
+++ var7-c3/3.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-    double x, y;
+    float x, y;
 
       printf("Введите первое число: ");
 	  scanf("%f", &x);
 	  printf("Введите второе число: ");
 	  scanf("%f", &y);
 	  
-	  if (x >= y) printf("Большим было число %f\n", x);
-        else printf("Большим было число %f\n", y);
+	    if (x >= y) printf("Большим было число %.3f\n", x);
+          else printf("Большим было число %.3f\n", y);
 	  
     getch();
 }
