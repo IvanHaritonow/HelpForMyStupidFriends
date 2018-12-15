d8e2517c072feb899ad78d73b9ccef13635a4b7b
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-15 18:15:54 +0300

    Update 2.c

--- var8-c4/2.c
+++ var8-c4/2.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int maxOf(int a, int b, int c) {
+    int max;
+    if (a >= b) max = a;
+       else max = b;
+    if (max <= c) max = c;
+    return max;
+}
+
+int minOf(int a, int b, int c) {
+    int min;
+    if (a <= b) min = a;
+       else min = b;
+    if (min >= c) min = c;
+    return min;
+}
+
 int main()
 {
-    int a, b, c;
-    printf("Enter a: ");
+    int a, b, c, sum;
+    printf("Введите a: ");
     scanf("%d/n", &a);
-    printf("Enter b: ");
+    printf("Введите b: ");
     scanf("%d/n", &b);
-    printf("Enter c: ");
+    printf("Введите c: ");
     scanf("%d/n", &c);
     
-    if (a + b >= c || a + c >= b || b + c >= a) 
-         printf("false"); else printf("true");
+    sum = a + b + c;
+    a = maxOf(a, b, c);
+    c = minOf(a, b, c);
+    b = sum - a - c;
+    
+    if (a + b > c && a - b < c) 
+         printf("Треуголник существует"); else printf("Треуголник не существует");
         
     getch();
 }
