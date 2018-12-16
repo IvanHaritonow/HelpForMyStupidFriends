22ecd90064a16662d84acc5d06e45ef403fbdaa5
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-16 23:12:22 +0300

    Update 1.c

--- var5-c4/1.c
+++ var5-c4/1.c
@@ -1,1 +1,17 @@
-code::yahoo.com
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int suit;
+    
+    printf("Введите число от 1 до 4: ");
+    scanf("%d", &suit);
+    
+       if (suit == 1) printf ("пики"); else
+       if (suit == 2) printf ("трефы"); else
+       if (suit == 3) printf ("бубны"); else
+       if (suit == 4) printf ("червы"); else
+         printf("Введённое число должно быть от 1 до 4");
+         
+    getch();
+}
