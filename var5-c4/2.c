8384ac8ff41653731b5d3b135b78a1aa794eb88e
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-16 23:40:38 +0300

    Update 2.c

--- var5-c4/2.c
+++ var5-c4/2.c
@@ -1,1 +1,19 @@
-code::google.com
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int year, check;
+    
+    printf("Введите год: ");
+    scanf("%d", &year);
+    
+        if (year % 400 == 0) check = 1; else
+        if (year % 100 == 0) check = 0; else
+        if (year % 4 == 0) check = 1; else
+            check = 0;
+        
+        if (check == 1) printf("%d-й год является високосным", year); 
+            else printf("%d-й год не является високосным", year);
+         
+    getch();
+}
