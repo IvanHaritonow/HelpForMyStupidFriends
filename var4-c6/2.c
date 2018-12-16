1156f3babd1b0a3d76f3b4aa237fda5deadd75bf
IvanHaritonow <36366093+IvanHaritonow@users.noreply.github.com>
2018-12-16 22:52:24 +0300

    Update 2.c

--- var4-c6/2.c
+++ var4-c6/2.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() { 
-	int i, x;
+	int x;
 	double y;
 	
-         for (i = -9; i <= 9; i += 3) {
-	    x = i;
-	  printf("При значении х = %d", x);
-	      y = 4 * x * x + 5 * x - 10;
-	  printf(" функция y(x) = %d\n", y);	
-   } 
+         for (x = -9; x <= 9; x += 3) {
+	     printf("При значении х = %d", x);
+	        y = 4 * x * x + 5 * x - 10;
+	     printf(" функция y(x) = %.0f\n", y);	
+         } 
   getch();
  }
