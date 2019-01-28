5b2bdbf1d4a387d12b5382e64565db0a164a0354
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-01-28 15:55:58 +0300

    Create 2.c

---
+++ var6-c2/2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+    int v, m;
+    float p;
+    
+    printf("Масса: ");
+    scanf("%d", &m);
+    printf("Объём: ");
+    scanf("%d", &v);
+    
+    p = m / v;
+    printf("Плотность = %f", p);
+    
+    getch();
+}
