972a122f7f7245e9d9774c6cce2914ad8642cfbd
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-01-28 17:00:14 +0300

    Create 1.c

---
+++ var4-c4/1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int dayOfMonth = 8, isBissextile;
+    
+    printf("Введите 1 если год високосный (иначе любой символ): ");
+    scanf("%d", &isBissextile);
+    if(isBissextile == 1 && dayOfMonth == 2) {
+            printf("29");
+            exit(0);
+    }
+    
+    switch (dayOfMonth) {
+    case 1: printf("31");
+    break;
+    case 2: printf("28");
+    break;
+    case 3: printf("31");
+    break;
+    case 4: printf("30");
+    break;
+    case 5: printf("31");
+    break;
+    case 6: printf("30");
+    break;
+    case 7: printf("31");
+    break;
+    case 8: printf("31");
+    break;
+    case 9: printf("30");
+    break;
+    case 10: printf("31");
+    break;
+    case 11: printf("30");
+    break;
+    case 12: printf("31");
+    break;
+    default: printf("Invalid Data");
+    break;
+    }
+    
+    getch();
+}
