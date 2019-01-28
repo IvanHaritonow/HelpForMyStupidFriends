317d6d1b7604e2706c820df0155c5b6da246074a
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-01-28 15:23:32 +0300

    Create 3.c

---
+++ var1-c4/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c;
+    
+    if (a < 3.8 && a > 1.6) 
+        printf("%f \n", a);
+    if (b < 3.8 && b > 1.6) 
+        printf("%f \n", b);
+    if (c < 3.8 && c > 1.6) 
+        printf("%f \n", c);
+        
+    getch();
+}
