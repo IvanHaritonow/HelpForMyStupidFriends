e084f75ac6e2edebe6d9df3f0c172815545a8787
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-01-28 14:44:22 +0300

    Create 2.c

---
+++ var6-c3/2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int anyNum;
+    
+    if (5 % anyNum == 0)
+        printf("Число является делителем числа 5");
+    else printf ("Число НЕ является делителем числа 5");   
+    
+    getch();
+}
