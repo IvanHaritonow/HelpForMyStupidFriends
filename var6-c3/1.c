1932669b3e6353aaa2ad74b5bfff0ae1a7ae45c8
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-02-08 23:06:25 +0300

    Update 1.c

--- var6-c3/1.c
+++ var6-c3/1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int Y;
-    int x;
+    int x = 10, Y;
     
     if (x < 3)
         Y = (4 * x ^ 2 + 7) / (x - 3);
         
     if (x >= 3)
         Y = 3 * x ^ 2 - 2;
-        
+    
+    printf("%d", Y);
     getch();
 }
