d8dd4f839a0cc092264eeb6d5eb7a26ea6756f1c
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-02-11 23:56:58 +0300

    Update 3.c

--- var8-c10/3.c
+++ var8-c10/3.c
@@ -1,1 +1,33 @@
+//***---Скорее всего не работает правильно, я хз, у меня компилятор с ума сходит---***
 
+#include <stdio.h>
+
+int main() {
+    
+    int j = 0, N = 10, M = 5, K = 5, k = 0;
+    int a[] = {};
+    int b[] = {1, 3, 5, 7, 9};
+    int c[] = {0, 2, 4, 6, 8};
+    
+    for (int i = 0; i < M; i++) 
+        a[i] = b[i];
+        
+    for (int i = 0; i < K; i++ )
+        a[i + K] = c[i];
+    
+    while (j != N) {
+        for (int i = 0; i < N - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                k = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = k;
+            }
+        }
+        j++;
+    }
+    
+    for (int i = 0; i < N; i++ )
+        printf("%d  ", a[i]);
+    
+    getch();
+}
