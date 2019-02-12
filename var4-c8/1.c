c7f7933a62a6b4f192511b81229156061f227c35
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-02-12 07:59:25 +0300

    Update 1.c

--- var4-c8/1.c
+++ var4-c8/1.c
@@ -2,14 +2,17 @@
 
 int main(){
     
-    int i;
-    int a[12] = {};
+    int i, n;
+    int a[] = {};
     
-    for(i = 0; i <= 11; i++) scanf("%d", &a[i]);
+    printf("Сколько элементов будет в массиве?\n");
+    scanf("%d", &n);
+    printf("Введите элементы по одному\n");
     
-    printf("%d %d %d %d %d %d %d %d %d %d %d %d\n",
-        a[0], a[1], a[2], a[3], a[4], a[5], 
-        a[6], a[7], a[8], a[9], a[10], a[11]);
+    for(i = 0; i < n; i++) scanf("%d", &a[i]);
+    
+    printf("\n");
+    for(i = 0; i < n; i++) printf("%d  ", a[i]);
     
     getch();
 }
