1edda5602313977d68055977c2bec8384db1651e
Ivanych <36366093+IvanHaritonow@users.noreply.github.com>
2019-02-12 08:08:38 +0300

    Update 2.c

--- var4-c8/2.c
+++ var4-c8/2.c
@@ -2,17 +2,19 @@
 
 int main() {
     int check = 0;
-    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int a[10] = {};
     
-    for(int i = 0; i < 10; i++) {
+    printf("Заполните массив: \n");
+    for(int i = 0; i < 10; i++)
+        scanf("%d", &a[i]);
+    
+    for(int i = 0; i < 10; i++)
         if(a[i] % 2 != 0) check++;
-    }
     
     printf("Количество нечётных элементов в массиве: %d\n", check); 
     printf("Список нечётных элементов в массиве: \n");
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++)
         if(a[i] % 2 != 0) printf("%d  ", a[i]);
-    }
     
     getch();
 }
